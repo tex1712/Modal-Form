0f213bceb4ebc19f150372141dac73f95458f8f0
tex1712 <30326668+tex1712@users.noreply.github.com>
2017-10-07 13:35:17 +0300

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,28 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['name'])) {$name = $_POST['name'];}
+    if (isset($_POST['phone'])) {$phone = $_POST['phone'];}
+    if (isset($_POST['formData'])) {$formData = $_POST['formData'];}
+
+    $to = "webuss.com.ua@gmail.com"; /*Укажите адрес, га который должно приходить письмо*/
+    $sendfrom   = "call@webuss.in.ua"; /*Укажите адрес, с которого будет приходить письмо, можно не настоящий, нужно для формирования заголовка письма*/
+    $headers  = "From: " . strip_tags($sendfrom) . "\r\n";
+    $headers .= "Reply-To: ". strip_tags($sendfrom) . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html;charset=utf-8 \r\n";
+    $subject = "$formData";
+    $message = "$formData<br> <b>Имя пославшего:</b> $name <br><b>Телефон:</b> $phone";
+    $send = mail ($to, $subject, $message, $headers);
+    if ($send == 'true')
+    {
+    echo '<center><p class="success color_element">Спасибо!</p><p class="success color_text">Ваша заявка успешно отправлена</p><p class="success color_element">Мы свяжимся с Вами в ближайшее время</p></center>';
+    }
+    else 
+    {
+    echo '<center><p class="fail"><b>Ошибка. Сообщение не отправлено!</b></p></center>';
+    }
+} else {
+    http_response_code(403);
+    echo "Попробуйте еще раз";
+}
+?>
